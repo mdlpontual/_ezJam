3d1fbdb45a48abf19c425965860c79d4f48416ee
mdlpontual <mdlpontual@gmail.com>
2024-07-01 20:28:54 -0300

    finishin the ui for the login page

--- src/components/ui/LoginContainer.jsx
+++ src/components/ui/LoginContainer.jsx
@@ -4,7 +4,37 @@ import IMG from "../../assets/images/ImagesHUB";
 function LoginContainer() {
     return(
         <>
-            
+            <div id="login-box" className="container">
+                <div id="logo-row" className="row">
+                    <div id="jam-logo" className="col">
+                        <img src={IMG.jammmingLogo} alt="jammming logo" width='480px' />  
+                    </div>
+                    <div id="spotify-logo" className="col">
+                        <p id="with-text">with</p>
+                        <a href="https://open.spotify.com/intl-pt">
+                            <img src={IMG.spotifyLogo} alt="spotify logo" width='100px' />
+                        </a>  
+                    </div>
+                </div>
+                <div id="button-row" className="row">
+                    <div id="button-col" className="col">
+                        <button id="login-button" className="btn btn-primary btn-lg" type="button">Login to Spotify</button>
+                    </div>
+                </div>
+                <div id="switch-row" className="row">
+                    <div id="switch-col" className="form-check form-switch">
+                        <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+                        <label className="form-check-label" for="flexSwitchCheckDefault">&nbsp;&nbsp;Remember-me</label>
+                    </div>
+                </div>
+                <div id="subscribe-row" className="row">
+                    <div id="subscribe-col" className="col">
+                        <a href="https://www.spotify.com/br-pt/signup?forward_url=https%3A%2F%2Fopen.spotify.com%2Fintl-pt">
+                            <p>Subscribe to Spotify</p>
+                        </a>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,29 +1,16 @@
 import React from "react";
 import LoginContainer from '../components/ui/LoginContainer.jsx';
-import IMG from '../assets/images/ImagesHUB.jsx';
+import LoginFooterSignature from "../components/ui/LoginFooterSignature.jsx";
 
 function LoginPage() {
     return (
         <>
             <div className="container-fluid">
                 <main id="login-main" className="row justify-content-center align-items-center">
-                    <div id="login-box" className="container">
-                        <div id="jam-logo" className="col">
-                            <img src={IMG.jammmingLogo} alt="jammming logo" width='480px' />  
-                        </div>
-                        <div id="spotify-logo" className="col">
-                            <p id="with-text">with</p>
-                            <a href="https://open.spotify.com/intl-pt">
-                                <img src={IMG.spotifyLogo} alt="spotify logo" width='100px' />
-                            </a>  
-                        </div>
-                    </div>
+                    <LoginContainer/>
                 </main>
                 <footer id="footer-row" className="row">
-                    <article id="app-signature">
-                        <h6 id="light-signature">Jammming - a Codecademy Practice Project</h6>
-                        <h6 id="bold-signature">copyright mdlpontual - 2024</h6>
-                    </article>
+                    <LoginFooterSignature/>
                 </footer>
             </div>
         </>
