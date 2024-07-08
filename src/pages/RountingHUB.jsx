ad4f1ebad18d17babccb77a8a864dc5c2dc192f2
mdlpontual <mdlpontual@gmail.com>
2024-07-08 16:25:38 -0300

    routing import corrections

--- src/pages/RountingHUB.jsx
+++ src/pages/RountingHUB.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
-import HomePage from './HomePage.jsx';
-import LoginPage from './LoginPage.jsx';
+import HomePage from './Home/HomePage.jsx';
+import LoginPage from './Login/LoginPage.jsx';
 import NotFoundPage from './NotFoundPage.jsx';
 
 const routes = createBrowserRouter([
