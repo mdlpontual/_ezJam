a0b373328c0c0f36659aea94b8fbf5d29402b706
mdlpontual <mdlpontual@gmail.com>
2024-07-18 17:56:22 -0300

    erase the files to redo them in a more organized way

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
+import React from 'react'
 
 function App() {
-  return <h1>testing</h1>;
+  
 }
 
 export default App
--- src/pages/Home/ui/HomeContainers.jsx
+++ src/pages/Home/ui/HomeContainers.jsx
@@ -1,16 +1,13 @@
 import React from "react";
-import UserPlaylist from "./Playlists/UserPlaylists";
-import OpenPlaylist from "./Playlists/OpenPlaylist";
-
 
 function HomeContainers(){
     return(
         <>
             <section id="playlists-box" className="col">
-                <UserPlaylist />
+
             </section>
             <section id="search-box" className="col">
-                <OpenPlaylist />
+                
             </section>
         </>
     )
