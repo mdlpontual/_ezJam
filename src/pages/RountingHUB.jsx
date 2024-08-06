6e3c893fdf8091a3c48f3509702096bcbbe2945a
mdlpontual <mdlpontual@gmail.com>
2024-08-06 13:45:52 -0300

    fix routing Hub links

--- src/pages/RountingHUB.jsx
+++ src/pages/RountingHUB.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
-import HomePage from './home/HomePage.jsx';
-import LoginPage from './login/LoginPage.jsx';
+import HomePage from './Home/HomePage.jsx';
+import LoginPage from './Login/LoginPage.jsx';
 import NotFoundPage from './NotFoundPage.jsx';
 
 const routes = createBrowserRouter([
