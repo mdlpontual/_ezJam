a7aecd9c4dccb49ba587bc4b94c10c123802b4a7
mdlpontual <mdlpontual@gmail.com>
2024-11-08 18:52:31 -0300

    about content

--- ccpp-jammming/src/components/ui/home/header/HomeHeader.jsx
+++ ccpp-jammming/src/components/ui/home/header/HomeHeader.jsx
@@ -20,7 +20,26 @@ function HomeHeader() {
                     </figure>
                     <nav id="header-nav-col" className="col d-flex justify-content-end align-items-center">
                         <ul className="d-flex align-items-center">
-                            <li><AboutPopover content="copyright mdlpontual - 2024"/></li>
+                            <li>
+                                <div>
+                                    <AboutPopover content={`
+                                    <p>In this application, you will find a better and improved way to customize your beloved Spotify's playlists!</p>
+                                    <br>
+                                    <p>Only with ezJam will you have the ability to simultaneously view the content of your playlists while exploring Spotify's music catalog, all at the same time, side by side!</p>
+                                    <br>
+                                    <p>Focusing solely on songs, albums, and artists, you will have a more streamlined experience to create, edit, and customize your music playlists. Less distractions means better flow and more exploration!</p>
+                                    <br>
+                                    <h5>- Select one or multiple tracks at the same time;</h5>
+                                    <h5>- Drag and drop to add new songs or to change their order;</h5>
+                                    <h5>- Maintain control of your customization by saving or discarding the changes you make along the way.</h5>
+                                    <br>
+                                    <p>After your customizations, you can play the playlists in your Spotify app or even share them with a simple quick click!</p>
+                                    <br>
+                                    <p>Enjoy your jams!</p>
+                                    <hr></hr>
+                                    <h6>copyright mdlpontual - 2024</h6>`}/>
+                                </div>
+                            </li>
                             <li><a href="">Logoff</a></li>
                         </ul>
                     </nav>
--- ccpp-jammming/src/utils/AboutPopover.jsx
+++ ccpp-jammming/src/utils/AboutPopover.jsx
@@ -5,19 +5,32 @@ function AboutPopover(props) {
     
     React.useEffect(() => {
         Array.from(document.querySelectorAll('button[data-bs-toggle="popover"]'))
-          .forEach(popoverNode => new Popover(popoverNode))
-    }) 
+            .forEach(popoverNode => {
+                const popoverInstance = new Popover(popoverNode);
+                popoverNode.addEventListener('shown.bs.popover', () => {
+                    const popoverElement = document.querySelector('.popover');
+                    if (popoverElement) {
+                        popoverElement.style.width = "fit-content"; // Allows popover to grow with content
+                        popoverElement.style.minWidth = "250px";    // Ensures a minimum width
+                    }
+                });
+            });
+    }, []);
  
     return(
         <div>
-            <button id="about-button" type="button" 
-            className="btn btn-primary" 
-            data-bs-container="body" 
-            data-bs-toggle="popover" 
-            data-bs-placement="bottom" 
-            title=""
-            data-bs-content={props.content}>
-                About
+            <button
+                id="about-button"
+                type="button"
+                className="btn btn-primary btn-block custom-popover"
+                data-bs-container="body"
+                data-bs-toggle="popover"
+                data-bs-placement="bottom"
+                data-bs-html="true"
+                title="<h4>Welcome to ezJam!</h4>"
+                data-bs-content={props.content}
+                onClick={(e) => e.stopPropagation()}>
+                    About
             </button>
         </div>
     )
