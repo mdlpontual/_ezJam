34fdcee40f571a09b7cd8df284658fba4af40b1c
mdlpontual <mdlpontual@gmail.com>
2024-11-10 21:26:31 -0300

    empty results page

--- ccpp-jammming/src/components/ui/home/search_container/non_results/EmptyResultsPage.jsx
+++ ccpp-jammming/src/components/ui/home/search_container/non_results/EmptyResultsPage.jsx
@@ -5,8 +5,10 @@ function EmptyResultsPage() {
     return (
         <>
             <div id="empty-results-col" className="col d-flex flex-column justify-content-center align-items-center">
-                <img src={IMG.spotifySearch2PNG} alt="spotify empty" width="200px"/>
+                <img src={IMG.notFound2PNG} alt="spotify empty" width="200px"/>
                 <h3 id="sptf">search by artist, album or title</h3>
+                <h5 id="label-one">all content provided by:</h5>
+                <img src={IMG.spotifyLogoWhite} alt="spotify empty" width="175px"/>
             </div>
         </>
     );
