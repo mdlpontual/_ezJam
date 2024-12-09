79c7b36d739abb6a7b3cdf5abcdbdb545a111992
mdlpontual <mdlpontual@gmail.com>
2024-12-09 16:32:15 -0300

    test 04

--- ccpp-jammming/src/main.jsx
+++ ccpp-jammming/src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min"
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./scss/main.scss"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
