dd655b4b2f34b45577b61694c42f9befd37217ce
mdlpontual <mdlpontual@gmail.com>
2024-12-09 17:40:24 -0300

    test 11

--- ccpp-jammming/vite.config.js
+++ ccpp-jammming/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import mkcert from 'vite-plugin-mkcert';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), mkcert()],
   server: {
@@ -14,25 +13,6 @@ export default defineConfig({
     sourcemap: true, // Enable source maps
   },
   optimizeDeps: {
-    include: ["bootstrap"],
+    include: ['bootstrap'], // Explicitly include Bootstrap
   },
-  base: './', // Ensure relative paths for production assets
 });
-
-
-/* import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import mkcert from 'vite-plugin-mkcert'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), mkcert()],
-  server: {
-    https: true,
-    port: 3000,
-    cors: true,
-  },
-  build: {
-    sourcemap: true, // Enable source maps
-  }
-}) */
