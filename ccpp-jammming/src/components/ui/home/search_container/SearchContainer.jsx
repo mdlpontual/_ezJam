e97ce8b1ce3b480568dcac50d93ff428e90d8a8a
mdlpontual <mdlpontual@gmail.com>
2024-12-10 11:29:40 -0300

    throttle on search bar

--- ccpp-jammming/src/components/ui/home/search_container/SearchContainer.jsx
+++ ccpp-jammming/src/components/ui/home/search_container/SearchContainer.jsx
@@ -1,4 +1,85 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
+import IMG from "../../../../assets/images/ImagesHUB";
+
+function SearchContainer({ search, setSearch, activePage, goBack, goForward }) {
+    
+    const disableEnter = (event) => {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+        }
+    };
+
+    // Custom throttle function
+    const throttle = (func, delay) => {
+        let lastCall = 0;
+        return (...args) => {
+            const now = new Date().getTime();
+            if (now - lastCall < delay) return;
+            lastCall = now;
+            return func(...args);
+        };
+    };
+
+    // Throttled version of setSearch
+    const throttledSetSearch = useCallback(
+        throttle((query) => setSearch(query), 500),
+        []
+    );
+
+    const handleInputChange = (event) => {
+        throttledSetSearch(event.target.value);
+    };
+
+    return (
+        <>
+            <div id="search-container" className="container-fluid d-flex flex-column">
+                <div id="searchbar-row" className="row">
+                    <div id="arrow-box-col" className="col-auto">
+                        <div id="arrow-container" className="container">
+                            <div id="arrow-row" className="row">
+                                <a id="go-back" type="button" className="col-auto d-flex flex-column justify-content-center align-items-center" onClick={goBack}>
+                                    <img title="Go to Previous Page" id="back-white" src={IMG.gobackPNG} alt="go back button" width="22px"/>
+                                    <img title="Go to Previous Page" id="back-green" src={IMG.gobackGreenPNG} alt="go back button" width="22px"/>
+                                </a>
+                                <a id="go-foward" type="button" className="col-auto d-flex flex-column justify-content-center align-items-center" onClick={goForward}>
+                                    <img title="Go to Next Page" id="foward-white" src={IMG.gofowardPNG} alt="go foward button" width="22px"/>
+                                    <img title="Go to Next Page" id="foward-green" src={IMG.gofowardGreenPNG} alt="go foward button" width="22px"/>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="searchbar-col" className="col d-flex flex-column justify-content-center align-items-center">
+                        <form id="form-elem" method="POST" className="container-fluid d-flex flex-column justify-content-center align-items-center">
+                            <div id="form-row" className="row justify-content-center align-items-center">
+                                <div id="search-button" className="col-2 d-flex justify-content-center align-items-center">
+                                    <img className="col" src={IMG.searchPNG} alt="search button" width="30px"/>
+                                </div>
+                                <input 
+                                    id="input-elem" 
+                                    type="search" 
+                                    placeholder="Search the Spotify Library" 
+                                    className="col" 
+                                    value={search} 
+                                    onChange={handleInputChange} 
+                                    onKeyDown={disableEnter}/>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div id="results-row" className="row">
+                    <div id="results-col" className="col d-flex">
+                        {activePage}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default SearchContainer;
+
+
+/* import React, { useState } from "react";
 import IMG from "../../../../assets/images/ImagesHUB";
 
 function SearchContainer({ search, setSearch, activePage, goBack, goForward }) {
@@ -55,4 +136,4 @@ function SearchContainer({ search, setSearch, activePage, goBack, goForward }) {
     );
 }
 
-export default SearchContainer;
+export default SearchContainer; */
